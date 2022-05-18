b1516a92a241baf4a4c352c2d10ca855edffd66c
Byjeso <jesuspalacioswp@gmail.com>
2022-05-18 15:46:55 -0500

    first commit

--- js/script.js
+++ js/script.js
@@ -22,9 +22,10 @@ window.addEventListener("load", () => {
       lat = posicion.coords.latitude;
 
       // ubicacion actual
-      // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4f6350d8d35448b1f6da59d57466bd59`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4f6350d8d35448b1f6da59d57466bd59`;
 
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=Valencia&lang=es&units=metric&appid=4f6350d8d35448b1f6da59d57466bd59`;
+      // ubicacion por ciudad
+      // const url = `https://api.openweathermap.org/data/2.5/weather?q=Bogota&lang=es&units=metric&appid=4f6350d8d35448b1f6da59d57466bd59`;
 
       //   console.log(url);
 
@@ -47,40 +48,7 @@ window.addEventListener("load", () => {
 
           // para iconos animados
           console.log(data.weather[0].main);
-          switch (data.weather[0].main) {
-            case "Thunderstorm":
-              iconoAnimado.src = "animated/thunder.svg";
-              console.log("TORMENTA");
-              break;
-            case "Drizzle":
-              iconoAnimado.src = "animated/rainy-2.svg";
-              console.log("LLOVIZNA");
-              break;
-            case "Rain":
-              iconoAnimado.src = "animated/rainy-7.svg";
-              console.log("LLUVIA");
-              break;
-            case "Snow":
-              iconoAnimado.src = "animated/snowy-6.svg";
-              console.log("NIEVE");
-              break;
-            case "Clear":
-              iconoAnimado.src = "animated/day.svg";
-              console.log("LIMPIO");
-              break;
-
-            case "Atmosphere":
-              iconoAnimado.src = "animated/weather.svg";
-              console.log("ATMOSFERA");
-              break;
-            case "Clouds":
-              iconoAnimado.src = "animated/cloudy-day-1.svg";
-              console.log("NUBES");
-              break;
-            default:
-              iconoAnimado.src = "animated/cloudy-day-1.svg";
-              console.log("por defecto");
-          }
+          iconoAnimado.src = `animated/${data.weather[0].main}.svg`;
 
           console.log(data);
         })
